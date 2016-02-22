c620fc460c4b2b676eeb12772443758d789f5e1b
brockuniera <brockuniera@utexas.edu>
2016-02-22 05:53:21 -0600

    Good amount of evaluation done

--- kb1.pl
+++ kb1.pl
@@ -1,3 +1,27 @@
+writeln(A) :- write(A), write('\n').
+
+parse(TokenList, AST) :- phrase(prog(AST), TokenList).
+evaluate(AST, Number) :- empty_assoc(Min), ev(AST, Number, Min, _).
+
+% Just eat progs, returns, bases
+ev(prog(return(A)), N, Min, Mout) :- ev(A, N, Min, Mout).
+ev(prog(A, B), N, Min, Mout) :- ev(A, N, Min, M1), ev(B, N, M1, Mout).
+ev(base(I), N, Min, Mout) :- ev(I, N, Min, Mout).
+ev(expr(T), N, Min, Mout) :- ev(T, N, Min, Mout).
+
+% Numbers allowed.
+ev(num(_), _, M, M).
+
+% Declarations. id cannot have existed before, and now it does. Mout != Min.
+ev(declr(id(I)), N, Min, Mout) :- \+ get_assoc(I, Min, _), put_assoc(I, Min, unassigned, Mout).
+
+% Assignments are valid when LHS id is in Min, and base is valid. Mout != Min.
+ev(assn(id(I), base(B)), N, Min, Mout) :- ev(B, N, Min, _), get_assoc(I, Min, _), put_assoc(I, Min, assigned, Mout).
+
+% Reading an id. The id has to exist and be assigned in Min.
+ev(id(I), N, Min, _) :- get_assoc(I, Min, assigned).
+
+
 prog(prog(R)) --> retStatement(R), [.].
 prog(prog(D, P)) --> declaration(D), [';'], prog(P).
 prog(prog(A, P)) --> assignment(A), [';'], prog(P).
@@ -27,12 +51,7 @@ mulOp(times) --> [*].
 mulOp(divide) --> [/].
 
 % < id > definition
-id(id(I)) -->
-    [I],
-    {
-        \+ member(I, [return, 'var']),
-        atom_codes(I, S), alphaword(S)
-    }.
+id(id(I)) --> [I], { \+ member(I, [return, 'var']), atom_codes(I, S), alphaword(S) }.
 alphaword([]).
 alphaword([H|T]) :- char_type(H, alpha), alphaword(T).
 
