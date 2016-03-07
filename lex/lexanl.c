4ee28603f170d10dba60582cb6f077a8f8dfa9f0
brockuniera <brockuniera@utexas.edu>
2016-03-07 05:41:48 -0600

    Add lex related files

---
+++ lex/lexanl.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()          /* Calls yylex repeatedly to test */
+  { 
+    int res, done;
+    printf("[");
+    done = 0;
+    while (done == 0)
+      { res = yylex();    /* yylex is the entry point to the lex program */
+        if (res == 0) done = 1;
+      }
+    printf("]\n");
+    return 0;
+  }
